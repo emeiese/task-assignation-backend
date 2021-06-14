be1bc64bd970956f302ffd6891c7b8c409f51e1b
emeiese <mcsanchez@uc.cl>
2021-06-13 20:35:39 -0400

    First commit :star:

---
+++ server/app.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI, Query, Body
+from typing import List, Dict, Optional
+from pydantic import BaseModel
+from server_functions import resolve_problem
+from math import floor
+
+# uvicorn app:app --reload
+
+app = FastAPI()
+
+
+class ProblemParams(BaseModel):
+    people: List[str]
+    tasks: List[str]
+    days: List[str]
+    costs: Dict[str, Dict[str, int]]
+    min_assign_task: int = 1
+    max_assign_task: int = 10000
+    max_total_assign: int = 10000
+    min_total_assign: int = 1
+
+
+@app.get("/")
+def read_root():
+    return {"Init_message": "Wena' mundooo"}
+
+
+@app.post("/getOptions/")
+async def get_options(tasks: List[str]):
+    """Given the tasks list, returns a list of dictionaries with the options for the fronetend
+
+    Args:
+        tasks (List[str]): List of tasks
+
+    Returns:
+        [List[Dict]]: Lists with options
+    """
+    size = len(tasks)
+    d_options = {str(v): v for v in range(0, size)}
+    options = []
+    for k, v in d_options.items():
+        option = {}
+        option["text"] = k
+        option["value"] = v
+        options.append(option)
+    return options
+
+
+@app.post("/checkCosts/")
+async def check_costs(costs: Dict[str, Dict[str, int]]):
+    """
+    Chequea si los costos para cada persona entregados por el usuario son válidos
+    """
+    correctness = {}
+    for human_name, tasks_costs in costs.items():
+        s = sum(v for (k, v) in tasks_costs.items())
+        if s != len(tasks_costs):
+            correctness[human_name] = False
+        else:
+            correctness[human_name] = True
+    return correctness
+
+
+@app.post("/resolve/")
+async def solve_problem(params: ProblemParams):
+    """
+    Resuelve el problema
+    """
+    return resolve_problem(params.people, params.tasks, params.days, params.costs, params.min_assign_task, params.max_assign_task, params.max_total_assign, params.min_total_assign)
+
+
+# Se le entrega una lista al path de la forma:
+# http://localhost:8000/items/?q=100&q=200
+# Además, tendrá un valor por defecto, que será [1, 2] si se entrega http://localhost:8000/items/
+
+# Los path params son los parámetros que van especificados entre {} al definir la ruta
+
+# En este caso, estamos especificando que 'q' es un query param opcional,
+# por lo que entregamos un valor por defecto
+
+# Query(...) declara explícitamente que ese es un query parameter.
+@app.get("/items/")
+async def read_items(q: Optional[List[int]] = Query([1, 2], title="Query item", description="Buena los cabros, aquí una descripción")):
+    query_items = {"q": q}
+    return query_items
---
+++ server/server_functions.py
@@ -0,0 +1,107 @@
+from pulp import LpProblem, lpSum, LpVariable, LpMinimize, LpStatus, value
+
+
+def resolve_problem(people, tasks, days, costs, min_assign_task, max_assign_task, max_total_assign, min_total_assign):
+    """Solves the optimization problem (optimal assignation for each task) 
+    given the parameters, costs and restriction values.
+
+    Args:
+        people ([List[str]]): People to consider on the optimization
+        tasks ([List[str]]): Tasks to consider on the optimization
+        days ([List[str]]): Days or periods to consider on the optimization
+        costs ([Dict[Dict[int]]]): Dictionary with the costs for each task for each human. Each key is a human name, while each subkey is a task.
+        min_assign_task ([int]): Indicates the minimum number of assignations that a person must have on a task on a week or at the end of the periods.
+        max_assign_task ([int]): Indicates the maximum number of assignations that a person can have on a task on a week or at the end of the periods.
+        max_total_assign ([int]): Indicates the maximum number of total assignations that a person can have on a week or at the end of the periods.
+        min_total_assign ([int]): Indicates the minimum number of total assignations that a person must have on a week or at the end of the periods.
+
+    Returns:
+        [Dict]: Returns a dict with the optimum assignation, the status of the problem and the value of the optimization function.
+    """
+
+    dpt_tuples = []
+    for d in days:
+        for p in people:
+            for t in tasks:
+                dpt_tuples.append((d, p, t))
+
+    day_task_tuples = []
+    for d in days:  
+        for t in tasks:  
+            day_task_tuples.append((d, t))
+
+    # Problem
+    prob = LpProblem("Assignation_Problem", LpMinimize)
+
+    # Variables
+    assign = LpVariable.dicts("assignation",
+                              ((day, person, task)
+                               for (day, person, task) in dpt_tuples),
+                              cat='Binary')
+
+    # Objective Function
+    prob += lpSum(
+        [assign[day, person, task] * costs[person][task] for (day, person, task) in dpt_tuples])
+
+    # Rstrictions
+
+    # No puede haber 1 persona en dos tareas el mismo día (si es que el número de tareas es menor al de personas).
+    if len(tasks) <= len(people):
+        for person in people:
+            for day in days:
+                prob += lpSum([assign[day, person, task]
+                              for task in tasks]) <= 1
+
+    # Todos los días, cada turno debe estar asignado a 1 person (esto implica implícitamente que habrán m turnos asignados por día).
+    for day in days:
+        for task in tasks:
+            prob += lpSum([assign[day, person, task]
+                          for person in people]) == 1
+
+    # Restricciones de Justicia:
+
+    # 1.Asignación mínima por tarea durante la semana para cada persona
+    for task in tasks:
+        for person in people:
+            prob += lpSum([assign[day, person, task]
+                          for day in days]) >= min_assign_task
+
+    # 2.Máximo de asignaciones en una semana, para una tarea, por persona
+    for person in people:
+        for task in tasks:
+            prob += lpSum([assign[day, person, task]
+                          for day in days]) <= max_assign_task
+
+    # 3.Número de asignaciones mínimas por persona durante una semana:
+    for person in people:
+        prob += lpSum([assign[day, person, task]
+                      for (day, task) in day_task_tuples]) >= min_total_assign
+
+    # 4.Número de asignaciones máximas por persona durante una semana:
+    for person in people:
+        prob += lpSum([assign[day, person, task]
+                      for (day, task) in day_task_tuples]) <= max_total_assign
+
+    print("-" * 50, "Resolviendo el problema", "-" * 50)
+    prob.solve()
+    status = LpStatus[prob.status]
+    val = value(prob.objective)
+    print("Status:", status)
+    print("Value:", val)
+    
+    if status == 'Optimal':
+        final = {}
+        for task in tasks:
+            assignations = {}
+            for day in days:
+                for person in people:
+                    aux = assign[(day, person, task)].varValue
+                    if aux:
+                        assignations[day] = person
+            final[task] = assignations
+    else:
+        final = None
+
+
+
+    return {"assignation": final, "status": status, "value": val}
