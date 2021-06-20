ae8813ca0fd92c4d37bfb2ff0e364821a150fe38
emeiese <mcsanchez@uc.cl>
2021-06-19 23:37:45 -0400

    Changing variable name and adding ceil function

--- server/backend.py
+++ server/backend.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Query, Body
 from typing import List, Dict, Optional
 from pydantic import BaseModel
 from server.server_functions import resolve_problem
-from math import floor
+from math import floor, ceil
 from fastapi.middleware.cors import CORSMiddleware
 
 # uvicorn server.backend:app --reload
@@ -17,8 +17,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def return_options(min_value, max_value):
+    d_options = {str(v): v for v in range(min_value, max_value + 1)}
+    options = []
+    for k, v in d_options.items():
+        option = {}
+        option["text"] = k
+        option["value"] = v
+        options.append(option)
+    return options
+
 class ProblemParams(BaseModel):
-    people: List[str]
+    names: List[str]
     tasks: List[str]
     days: List[str]
     costs: Dict[str, Dict[str, int]]
@@ -27,6 +37,11 @@ class ProblemParams(BaseModel):
     max_total_assign: int = 10000
     min_total_assign: int = 1
 
+class ParamsLength(BaseModel):
+    d: int
+    t: int
+    p: int
+    
 
 @app.get("/")
 def read_root():
@@ -43,6 +58,7 @@ async def get_options(tasks: List[str] = Body(..., embed=True)):
     Returns:
         [List[Dict]]: Lists with options
     """
+    """ 
     size = len(tasks)
     d_options = {str(v): v for v in range(0, size + 1)}
     options = []
@@ -51,6 +67,8 @@ async def get_options(tasks: List[str] = Body(..., embed=True)):
         option["text"] = k
         option["value"] = v
         options.append(option)
+    """
+    options = return_options(0, len(tasks))
     return options
 
 
@@ -69,13 +87,44 @@ async def check_costs(costs: Dict[str, Dict[str, int]] = Body(..., embed=True)):
             correctness[human_name] = True
     return True
 
+@app.post("/get_restriction_options")
+async def get_restriction_options(params: ParamsLength):
+    """Retorna diccionarios con las opciones posibles para las restricciones
+
+    Args:
+        d (int): cantidad total de días del problema 
+        t (int): cantidad total de tareas del problema 
+        p (int): cantidad total de personas del problema 
+
+    Returns:
+        [type]: [description]
+    """
+    # Mínimo y máximo de valores para asignar en la primera restricción
+    #max_one = ceil(d / t)
+    max_one = 10
+    min_one = 1
+
+    # Mínimo y máximo de valores para asignar en la segunda restricción
+    max_two = 10
+    min_two = 1
+
+    # Mínimo y máximo de valores para asignar en la tercera restricción
+    max_three = params.t * params.d
+    min_three = floor((params.d * params.t) / params.p) + 1
+
+    one_options = return_options(min_one, max_one)
+    two_options = return_options(min_two, max_two)
+    three_options = return_options(min_three, max_three)
+
+    return {"1": one_options, "2": two_options, "3": three_options}
+
 
 @app.post("/resolve/")
 async def solve_problem(params: ProblemParams):
     """
     Resuelve el problema
     """
-    return resolve_problem(params.people, params.tasks, params.days, params.costs, params.min_assign_task, params.max_assign_task, params.max_total_assign, params.min_total_assign)
+    return resolve_problem(params.names, params.tasks, params.days, params.costs, params.min_assign_task, params.max_assign_task, params.max_total_assign, params.min_total_assign)
 
 
 # Se le entrega una lista al path de la forma:
